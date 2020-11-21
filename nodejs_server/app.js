058a252e84017b4781834772b8da6b9b91cdacca
hisham-maged10 <Hisham.maged10@gmail.com>
2020-11-21 04:48:52 +0200

    added configmap try

--- nodejs_server/app.js
+++ nodejs_server/app.js
@@ -2,7 +2,9 @@ let express = require("express");
 let app = express();
 
 app.get("/",(req,res) => {
-    res.send("Yolo edited 6 my man");
+    //res.send("Yolo edited 7 my man");
+    res.send(process.env.HISHAM);
+    console.log(process.env.HISHAM);
     console.log("Entered after edit 4 dude")
 });
 
